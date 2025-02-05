19208032012b603448bd5fab4abbacfbb6d788d2
putraamir <d097839@siswa.upsi.edu.my>
2025-02-06 01:55:29 +0800

    hosting files

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,15 +1,18 @@
 <?php
 
+ini_set('memory_limit', '256M');
+
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
+    ->withProviders()
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
@@ -17,7 +20,9 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         //
+
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
+
     })->create();
